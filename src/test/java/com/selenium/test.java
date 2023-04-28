5184418c41d57247d383055453ed27fc6af343a8
Eddy <edmundgoh2014@gmail.com>
2023-04-28 23:50:57 +0800

    added some comments

--- src/test/java/com/selenium/test.java
+++ src/test/java/com/selenium/test.java
@@ -21,9 +21,10 @@ public class test
         driver.navigate().to("http://the-internet.herokuapp.com/?ref=hackernoon.com");
         driver.manage().window().maximize();
 
-
+        //Open Basic Auth Link
         driver.findElement(By.xpath("//*[@id='content']/ul/li[3]/a")).click();
 
+        //Get the current URL and insert username and password to authenticate
         String oriBasicAuthURL = driver.getCurrentUrl();
         String newBasicAuthURL = oriBasicAuthURL.replace("http://", "");
         String username = "admin";
@@ -32,6 +33,7 @@ public class test
         String URL = "http://" + username + ":" + password + "@" + newBasicAuthURL;
         driver.navigate().to(URL);
 
+        //returns that the authentication is successful
         String actualString = driver.findElement(By.xpath("//*[@id='content']/div/p")).getText();
         assertTrue(actualString.contains("Congratulations! You must have the proper credentials."));
         driver.quit();
