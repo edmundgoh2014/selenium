ff3552a26e129d11ca7df8f04e285c0b82091a51
Eddy <edmundgoh2014@gmail.com>
2023-05-07 15:28:44 +0800

    added test setup, teardown and drag&drop test

--- src/test/java/com/selenium/test.java
+++ src/test/java/com/selenium/test.java
@@ -2,8 +2,12 @@ package com.selenium;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,13 +20,25 @@ public class test
     /**
      * Rigorous Test :-)
      */
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // This code will be executed before each test case
+        driver = new ChromeDriver();
+        driver.navigate().to("http://the-internet.herokuapp.com/?ref=hackernoon.com");
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        // This code will be executed after each test case
+        driver.quit();
+    }
+    
     @Test
     public void basicAuthPassed()
     {
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://the-internet.herokuapp.com/?ref=hackernoon.com");
-        driver.manage().window().maximize();
-
         //Open Basic Auth Link
         driver.findElement(By.xpath("//*[@id='content']/ul/li[3]/a")).click();
 
@@ -38,16 +54,11 @@ public class test
         //returns that the authentication is successful
         String actualString = driver.findElement(By.xpath("//*[@id='content']/div/p")).getText();
         assertTrue(actualString.contains("Congratulations! You must have the proper credentials."));
-        driver.quit();
     }
 
     @Test
     public void checkBrokenImagesReturnTrue()
     {
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("http://the-internet.herokuapp.com/?ref=hackernoon.com");
-        driver.manage().window().maximize();
-
         //Open Broken Image Link
         driver.findElement(By.xpath("//*[@id='content']/ul/li[4]/a")).click();
 
@@ -64,6 +75,43 @@ public class test
 
         //returns that there is a broken image on this webpage
         assertEquals(true, brokeImage);
-        driver.quit();
+    }
+
+    @Test
+    public void dragAndDrop()
+    {
+        //Open drag & drop link
+        driver.findElement(By.xpath("//*[@id='content']/ul/li[10]/a")).click();
+
+        WebElement columnA = driver.findElement(By.id("column-a"));
+        WebElement columnB = driver.findElement(By.id("column-b"));
+
+        //using JavascriptExecutor method to perform drag and drop function because WebDriver drag and drop method is not working
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
+                    + "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n" + "data: {},\n"
+                    + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
+                    + "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n" + "return event;\n"
+                    + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
+                    + "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
+                    + "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
+                    + "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n" + "}\n"
+                    + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
+                    + "var dragStartEvent =createEvent('dragstart');\n" + "dispatchEvent(element, dragStartEvent);\n"
+                    + "var dropEvent = createEvent('drop');\n"
+                    + "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
+                    + "var dragEndEvent = createEvent('dragend');\n"
+                    + "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
+                    + "var source = arguments[0];\n" + "var destination = arguments[1];\n"
+                    + "simulateHTML5DragAndDrop(source,destination);", columnA, columnB);
+
+        // Verify that the drop was successful
+        String columnA_actualText = columnA.getText();
+        String columnA_expectedText = "B";
+        if (columnA_actualText.equals(columnA_expectedText)) {
+            System.out.println("Drag and drop successful!");
+        } else {
+            System.out.println("Drag and drop failed.");
+        }
     }
 }
